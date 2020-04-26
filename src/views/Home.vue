491dc29e26b18f2418443d3f0ab36a0f47797e9c
peterbrwts <peterberwouts@icloud.com>
2020-04-26 14:42:43 +0200

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h1>dit is een test</h1>
+    <h1>dit is een test, nog meer test</h1>
   </div>
 </template>
 
